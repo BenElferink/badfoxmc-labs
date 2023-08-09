9316865353831d26781ca6692f2d6f2e004e3dab
Ben Elferink <ben.elferink@icloud.com>
2023-08-09 17:32:23 +0300

    added constants

--- constants/index.ts
+++ constants/index.ts
@@ -12,10 +12,23 @@ export const API_KEYS = {
 }
 
 export const POLICY_IDS = {
+  ADA_HANDLE: 'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a',
   BAD_KEY: '80e3ccc66f4dfeff6bc7d906eb166a984a1fc6d314e33721ad6add14',
+  BAD_FOX_2D: 'fa669150ad134964e86b2fa7275a12072f61b438d0d44204d3a2f967',
+  BAD_FOX_3D: '8804474d85430846883b804375b26b17c563df2338ea9b46652c3164',
+  BAD_MOTORCYCLE_2D: 'ab662f7402af587e64d217995e20f95ac3ae3ff8417d9158b04fbba8',
 }
 
 export const WALLET_ADDRESSES = {
+  TREASURY:
+    'addr1q9p9yq4lz834729chxsdwa7utfp5wr754zkn6hltxz42m594guty04nldwlxnhw8xcgd5pndaaqzzu5qzyvnc8tlgdsqtazkyh',
+  ROYALTIES:
+    'addr1qyv7wgxd4fjvp9jxr2v6tdpygmjxwatesaemvassemq6jq2rqhw6rvndlmdnp0y7mwvaux4v2wpz5rusyy8c636az70sjxtwe6',
+
+  MINT_BAD_FOX_2D: 'addr1vytm0f6n564th94cld4xgzr0g8xp4s2j07ww33qn4x2ss6gmmdzlm',
+  MINT_BAD_MOTORCYCLE_2D: 'addr1v8l4qgz688jxgerq788kp3xv7qdjymchddrv3dxyug5e3pg83anxd',
+  MINT_BAD_KEY: 'addr1v9tce86r8v9larevjr7el7d5ua3eruz2cn4d93mqmt8w4agmy2leh',
+
   GIVEAWAYS: 'addr1v9qxndxhkl2g5qsz5mdv0w07zau5gpp5vttw0perr2py9ugrr25fs',
 }
 
@@ -52,6 +65,7 @@ export const LS_KEYS = {
   WALLET_PROVIDER: 'WALLET_PROVIDER',
 }
 
+export const ADA_SYMBOL = '₳'
 export const ONE_MILLION = 1000000
 export const DECIMALS = {
   ADA: 6,
