2b9963ea3527c8f87ba55575884eaacc7ddba835
Ben Elferink <ben.elferink@icloud.com>
2023-08-14 14:59:10 +0300

    fixed media viewer

--- components/MediaViewer.tsx
+++ components/MediaViewer.tsx
@@ -1,29 +1,21 @@
 import Image from 'next/image'
 import { useMemo } from 'react'
-import type { MediaType } from '@/@types'
 import { MEDIA_TYPES } from '@/constants'
+import type { MediaType } from '@/@types'
 
 const MediaViewer = (props: { mediaType: MediaType; src: string; size?: string; withBorder?: boolean }) => {
   const { mediaType, src, size, withBorder } = props
 
   const className = useMemo(
-    () =>
-      (size ? size : 'w-[100px] h-[100px]') +
-      ' object-contain rounded-lg ' +
-      (withBorder ? 'm-1 border border-zinc-600' : ''),
+    () => (size ? size : 'w-[100px] h-[100px]') + ' object-contain rounded-lg ' + (withBorder ? 'm-1 border border-zinc-600' : ''),
     [size, withBorder]
   )
 
+  const w = Number(className.split(' ')[0]?.replace('w-[', '')?.replace('px]', ''))
+  const h = Number(className.split(' ')[1]?.replace('h-[', '')?.replace('px]', ''))
+
   return mediaType === MEDIA_TYPES['IMAGE'] ? (
-    <Image
-      src={src}
-      alt=''
-      className={className}
-      width={Number(className.split(' ')[0].replace('w-[', '').replace('px]', ''))}
-      height={Number(className.split(' ')[1].replace('h-[', '').replace('px]', ''))}
-      priority
-      unoptimized
-    />
+    <Image src={src} alt='' className={className} width={Number.isNaN(w) ? 1000 : w} height={Number.isNaN(h) ? 1000 : h} priority unoptimized />
   ) : mediaType === MEDIA_TYPES['VIDEO'] ? (
     <video src={src} controls className={className} />
   ) : mediaType === MEDIA_TYPES['AUDIO'] ? (
