cdcdd6ad426efb982648883343f1d926706e215a
Ben Elferink <ben.elferink@icloud.com>
2023-08-20 22:28:00 +0300

    improved UI on sdk iframe

--- public/sdk.js
+++ public/sdk.js
@@ -88,18 +88,18 @@ class BadLabsSDK {
     const isMobile = window.innerWidth <= 768
 
     this.iFrameWrapper = document.createElement('div')
-    this.iFrameWrapper.setAttribute('id', 'bad-labs-iframe-wrapper')
+    this.iFrameWrapper.setAttribute('id', `bad-labs-${this.product}-iframe-wrapper`)
     this.iFrameWrapper.setAttribute(
       'style',
       'width: 100vw; height: 100vh; background-color: rgba(0, 0, 0, 0.5); border: none; display: flex; align-items: center; justify-content: center; position: fixed; top: 0; left: 0; z-index: 999999'
     )
 
     this.iFrame = document.createElement('iframe')
-    this.iFrame.setAttribute('id', 'bad-labs-iframe')
+    this.iFrame.setAttribute('id', `bad-labs-${this.product}-iframe`)
     this.iFrame.setAttribute('src', src)
     this.iFrame.setAttribute(
       'style',
-      `max-width: ${isMobile ? '100vw' : '555px'}; width: 100%; max-height: ${
+      `max-width: ${isMobile ? '100vw' : '800px'}; width: 100%; max-height: ${
         isMobile ? '100vh' : '70vh'
       }; height: 100%; border: none; border-radius: ${isMobile ? '0' : '1rem'}; box-shadow: 0 0 2px #fff;`
     )
