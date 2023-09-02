c54ee1292aef7ec02bb483841c9375f90f143cb5
Ben Elferink <ben.elferink@icloud.com>
2023-09-02 15:47:36 +0300

    fix button styles

--- components/form/Button.tsx
+++ components/form/Button.tsx
@@ -18,7 +18,7 @@ const Button: (props: {
       type='button'
       disabled={disabled}
       onClick={() => !!onClick && onClick()}
-      className='w-[calc(100%-0.5rem)] m-1 p-4 flex items-center justify-center rounded-lg bg-zinc-600 hover:bg-zinc-500 disabled:text-zinc-600 disabled:bg-zinc-800 disabled:hover:bg-zinc-800 disabled:cursor-not-allowed'
+      className='w-[calc(100%-0.5rem)] m-1 p-4 flex items-center justify-center rounded-lg border border-transparent hover:border-zinc-400 focus:border-zinc-400 disabled:border-transparent bg-zinc-700 hover:bg-zinc-600 disabled:text-zinc-600 disabled:bg-zinc-800 disabled:hover:bg-zinc-800 disabled:cursor-not-allowed'
     >
       {Icon ? <Icon className='w-6 h-6 mr-2' /> : null}
       {label}
