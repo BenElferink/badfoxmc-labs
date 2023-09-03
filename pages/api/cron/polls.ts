a12832a539602a1e754430a8d4a01d00d0948c12
Ben Elferink <ben.elferink@icloud.com>
2023-09-03 11:59:57 +0300

    adde cron job

---
+++ pages/api/cron/polls.ts
@@ -0,0 +1,59 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { firestore } from '@/utils/firebase'
+import type { Poll } from '@/@types'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req
+
+  try {
+    switch (method) {
+      case 'GET': {
+        const now = Date.now()
+
+        const collection = firestore.collection('polls')
+        const collectionQuery = await collection.where('active', '==', true).get()
+
+        const docsThatNeedToUpdate = collectionQuery.docs
+          .map((doc) => {
+            const data = doc.data() as Poll
+
+            return {
+              ...data,
+              active: now < data.endAt,
+              id: doc.id,
+            }
+          })
+          .filter((item) => !item.active)
+
+        const batch = firestore.batch()
+
+        for await (const doc of docsThatNeedToUpdate) {
+          const { id } = doc
+
+          const updateBody: Partial<Poll> = {
+            active: false,
+            fungibleHolders: [],
+            nonFungibleUsedUnits: [],
+            entries: [],
+          }
+
+          batch.update(collection.doc(id), updateBody)
+        }
+
+        await batch.commit()
+
+        return res.status(204).end()
+      }
+
+      default: {
+        res.setHeader('Allow', 'GET')
+        return res.status(405).end()
+      }
+    }
+  } catch (error) {
+    console.error(error)
+    return res.status(500).end()
+  }
+}
+
+export default handler
