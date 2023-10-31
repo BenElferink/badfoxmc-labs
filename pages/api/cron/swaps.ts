518c65d9d4db4a0029064d220ffdd0e819947b9e
Ben Elferink <ben.elferink@icloud.com>
2023-10-31 10:30:28 +0200

    updated cron

--- pages/api/cron/swaps.ts
+++ pages/api/cron/swaps.ts
@@ -10,24 +10,37 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     switch (method) {
       case 'GET': {
+        const now = new Date().getTime()
+        const waitTime = 900000
+
         const collection = firestore.collection('swaps')
         const { docs } = await collection.get()
 
-        const swaps = (
-          docs.map((doc) => ({
-            ...doc.data(),
-            id: doc.id,
-          })) as Swap[]
-        ).filter((doc) => !doc.withdraw.txHash && !!doc.deposit.txHash)
+        const swaps = docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        })) as (Swap & { id: string })[]
 
-        if (swaps.length) {
-          console.warn(`found ${swaps.length} bad swaps`)
+        const swapsThatNeedToWithdraw = swaps.filter((doc) => !doc.withdraw.txHash && !!doc.deposit.txHash && now - doc.timestamp >= waitTime)
+        const swapsThatNeedToDelete = swaps.filter((doc) => !doc.withdraw.txHash && !doc.deposit.txHash && now - doc.timestamp >= waitTime)
 
-          for await (const { id } of swaps) {
+        if (swapsThatNeedToWithdraw.length) {
+          console.warn(`found ${swapsThatNeedToWithdraw.length} swaps that need to withdraw`)
+
+          for await (const { id } of swapsThatNeedToWithdraw) {
             await axios.post('https://labs.badfoxmc.com/api/swap', { docId: id })
-            await sleep(5000)
+            await sleep(2000)
           }
         }
+
+        if (swapsThatNeedToDelete.length) {
+          console.warn(`found ${swapsThatNeedToDelete.length} swaps that need to delete`)
+
+          for await (const { id } of swapsThatNeedToDelete) {
+            await collection.doc(id).delete()
+          }
+        }
+
         return res.status(204).end()
       }
 
