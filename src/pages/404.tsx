1c8a5f69b58cf0afb8d5f23a16bcf2f0493aa131
Ben Elferink <ben.elferink@icloud.com>
2024-08-23 16:11:26 +0300

    changed 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ const Page = () => {
   return (
     <div className='w-full flex flex-col items-center'>
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src='/media/404.webp' alt='404' className='mt-[10%] drop-shadow-[0_0_1px_rgba(255,255,255,1)]' />
+      <img src='https://badfoxmc.com/media/404.png' alt='404' className='mt-[5%] max-w-[70vw] max-h-[70vh] drop-shadow-[0_0_1px_rgba(255,255,255,1)]' />
     </div>
   )
 }
