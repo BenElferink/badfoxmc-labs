75fea7902db349957683d73f1fd7cf1c08ae21a5
Ben Elferink <ben.elferink@icloud.com>
2024-08-23 16:29:52 +0300

    fix IPFS links

--- src/functions/formatters/formatIpfsReference.ts
+++ src/functions/formatters/formatIpfsReference.ts
@@ -10,7 +10,7 @@ const formatIpfsReference = (str: string) => {
   const strHasIpfsPrefix = str.indexOf('ipfs://') === 0
 
   const ipfs = strIsUrl ? '' : strHasIpfsPrefix ? str : `ipfs://${str}`
-  const url = strIsUrl ? str : ipfs.replace('ipfs://', 'https://d28yzo4ezrm37i.cloudfront.net/image/') + '?s=400'
+  const url = strIsUrl ? str : ipfs.replace('ipfs://', 'https://ipfs5.jpgstoreapis.com/ipfs/') + '?s=400'
 
   return {
     ipfs,
